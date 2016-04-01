d58eb66489702a1ecd6fbf775e87c54e94f8dcd1
studiozut <studiozut@gmail.com>
2016-04-01 17:28:04 -0400

    updated configuration

---
+++ hello_world/src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,28 @@
+le
+ * Contains \Drupal\hello_world\Plugin\Block\ExampleEmptyBlock.
+ */
+
+namespace Drupal\hello_world\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Example: empty block' block.
+ *
+ * @Block(
+ *   id = "example_empty",
+ *   admin_label = @Translation("Example: empty block")
+ * )
+ */
+class ExampleEmptyBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // We return an empty array on purpose. The block will thus not be rendered
+    // on the site. See BlockExampleTest::testBlockExampleBasic().
+    return array();
+  }
+
+}
